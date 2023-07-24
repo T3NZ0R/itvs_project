bc7194dfaefc599e87d8b40ee0b8a39fa750fb48
t3nz0r <t3nz000r@gmail.com>
2023-07-24 15:05:29 +0300

    Header

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {styled, Typography} from "@mui/material";
 
 const AboutUs = () => {
     return (
         <>
-            <TypographyStyled variant={"subtitle1"}>AboutUs</TypographyStyled>
+            AboutUs
         </>
     );
 };
 
 export default AboutUs;
 
-const TypographyStyled = styled(Typography)`
-font-size: 100px;
-`
+
