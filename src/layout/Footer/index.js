9e7f98a6326c5b17c97a84bcae65bae62e2f56b7
t3nz0r <t3nz000r@gmail.com>
2023-07-24 15:07:21 +0300

    Header

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "../../styles/footer.module.scss"
+import Image from "next/image";
 
 const Footer = () => {
 
@@ -18,14 +19,14 @@ const Footer = () => {
                             <p className={styles.contactsPhoneTitle}>Факс: </p> (032) 258-26-63
                         </h4>
                         <div className={styles.footerSocialWrapper}>
-                            <img className={styles.footerSocialItem} src={"icons/facebook.svg"} alt="facebook"/>
-                            <img className={styles.footerSocialItem} src={"icons/instagramCircle.svg"} alt="instagram"/>
-                            <img className={styles.footerSocialItem} src={"icons/google.svg"} alt="google"/>
+                            <Image className={styles.footerSocialItem} src={"icons/facebook.svg"} alt="facebook"/>
+                            <Image className={styles.footerSocialItem} src={"icons/instagramCircle.svg"} alt="instagram"/>
+                            <Image className={styles.footerSocialItem} src={"icons/google.svg"} alt="google"/>
                         </div>
                     </div>
                     <div className={styles.footerLocation}>
-                        <img className={styles.locationMap} src={"img/Map.png"} alt="Map"/>
-                        <h3 className={styles.locationText}><img className={styles.locationIcon} src={"icons/location.svg"} alt="location"
+                        <Image className={styles.locationMap} src={"img/Map.png"} alt="Map"/>
+                        <h3 className={styles.locationText}><Image className={styles.locationIcon} src={"icons/location.svg"} alt="location"
                                                                  /> вул. С.Бандери, 28а, Львів-13, 79013,V
                             навчальний корпус, кім.514.</h3>
                     </div>
@@ -35,13 +36,13 @@ const Footer = () => {
                     <a className={styles.footerLink} href="#">
                         <div className={styles.footerLinkTitle}>Дизайн сайту:
                             <div className={styles.footerLinkInfo}>
-                                <img src={"icons/instagram.svg"} alt="instagram"/> @yurii_oleskiv
+                                <Image src={"icons/instagram.svg"} alt="instagram"/> @yurii_oleskiv
                             </div></div>
                     </a>
                     <a className={styles.footerLink} target={"_blank"} href="https://linktr.ee/t3nz0r">
                         <div className={styles.footerLinkTitle}>Розробка сайту:
                             <div className={styles.footerLinkInfo}>
-                                <img src={"icons/instagram.svg"} alt="instagram"/> @t3nz0r
+                                <Image src={"icons/instagram.svg"} alt="instagram"/> @t3nz0r
                             </div>
                         </div>
                     </a>
