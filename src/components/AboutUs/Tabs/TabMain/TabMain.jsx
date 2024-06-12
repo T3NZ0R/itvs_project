320d1c1c4b4324a3cfb5307897963eda1eeac76a
Vitalii <vitalii@discountninja.io>
2024-06-12 14:21:33 +0300

    feat: Fix Images

--- src/components/AboutUs/Tabs/TabMain/TabMain.jsx
+++ src/components/AboutUs/Tabs/TabMain/TabMain.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from '../../../../styles/tabMain.module.scss'
-import localFont from "next/font/local";
-const myFont = localFont({src: '../../../../../public/fonts/KyivTypeSans-Black-.otf'})
 
 const TabMain = ({title, info, additionalSection}) => {
     return (
         <div className={styles.TabMainInfo}>
-            <h2 className={`${myFont.className} ${styles.TabMainInfoTitle}`}>{title}</h2>
+            <h2 className={ styles.TabMainInfoTitle}>{title}</h2>
             {additionalSection && additionalSection}
             <p className={styles.TabMainInfoSubtitle}>
                 {info}
