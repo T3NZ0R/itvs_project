a0497129789b07f7c12480a0cb63f269ee1b7fd7
Vitalii <vitalii@discountninja.io>
2024-06-12 14:19:45 +0300

    feat: Fix Images

--- src/layout/AppLayout/index.js
+++ src/layout/AppLayout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Analytics } from "@vercel/analytics/react"
+import {Helmet} from "react-helmet";
 
 import Header from "../Header";
 import Footer from "../Footer";
@@ -7,6 +8,9 @@ import Footer from "../Footer";
 const AppLayout = ({children}) => {
     return (
         <>
+            <Helmet>
+                <title>Кафедра ІТВС</title>
+            </Helmet>
             <Analytics/>
             <Header/>
             <main>
